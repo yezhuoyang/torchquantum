1b38709b9cc36737a2e407c7237ca32691c03d97
yezhuoyang <yezhuoyang@sjtu.edu.cn>
2022-10-10 20:12:36 +0800

    Add DensityMatrix class

---
+++ torchquantum/density.py
@@ -0,0 +1,19 @@
+import torch
+import torchquantum as tq
+import torchquantum.functional as tqf
+import itertools
+import functools
+import numpy as np
+
+from torchquantum.macro import C_DTYPE, ABC, ABC_ARRAY
+from torchpack.utils.logging import logger
+
+
+__all__ = ['DensityMatrix']
+
+class DensityMatrix(nn.Module):
+
+    def __init__():
+        return
+
+    
